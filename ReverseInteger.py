fe1ae920fea0bceea2c76b96fb31d8dfe45b93b5
rohanakut <rohan.akut97@gmail.com>
2020-09-05 20:38:53 +0530

    Added explanation in evernote

---
+++ ReverseInteger.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        flag = 0
+        if(x<0):
+            flag = 1
+            x *= (-1)
+        s = str(x)
+        s = s[::-1]
+        x = int(s)
+        #print(s,x)
+        if(x>2147483648):
+            return 0
+        if(flag == 1):
+            x*=(-1)
+        return x
